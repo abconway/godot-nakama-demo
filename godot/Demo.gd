3b911fdfa2ed30c5a9bfcc698b4b37f31f0d27c9
Alex Conway <aconway@squarespace.com>
2023-01-06 13:32:50 -0500

    Add basic server socket connection
    - Connect to the nakama server via a socket

--- godot/Demo.gd
+++ godot/Demo.gd
@@ -4,7 +4,13 @@ extends Node2D
 onready var server_connection := $ServerConnection
 onready var debug_panel := $CanvasLayer/DebugPanel
 
+
 func _ready() -> void:
+	yield(request_authentication(), "completed")
+	yield(connect_to_server(), "completed")
+
+
+func request_authentication() -> void:
 	var email := "test@test.com"
 	var password := "password"
 	
@@ -15,3 +21,11 @@ func _ready() -> void:
 		debug_panel.write_message("Authenticated user %s successfully." % email)
 	else:
 		debug_panel.write_message("Could not authenticate user %s." % email)
+
+
+func connect_to_server() -> void:
+	var result: int = yield(server_connection.connect_to_server_async(), "completed")
+	if result == OK:
+		debug_panel.write_message("Connected to the server.")
+	elif ERR_CANT_CONNECT:
+		debug_panel.write_message("Could not connect to server.")
--- godot/ServerConnection.gd
+++ godot/ServerConnection.gd
@@ -3,6 +3,8 @@ extends Node2D
 const KEY := "defaultkey"
 var _session: NakamaSession
 var _client:= Nakama.create_client(KEY, "127.0.0.1", 7350, "http")
+var _socket: NakamaSocket
+
 
 func authenticate_async(email: String, password: String) -> int:
 	var result := OK
@@ -14,3 +16,16 @@ func authenticate_async(email: String, password: String) -> int:
 		result = new_session.get_exception().status_code
 	
 	return result
+
+
+func connect_to_server_async() -> int:
+	_socket = Nakama.create_socket_from(_client)
+	var result : NakamaAsyncResult = yield(_socket.connect_async(_session), "completed")
+	if not result.is_exception():
+		_socket.connect("closed", self, "_on_NakamaSocket_closed")
+		return OK
+	return ERR_CANT_CONNECT
+
+
+func _on_NakamaSocket_closed() -> void:
+	_socket = null
